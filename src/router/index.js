08d308eb9f412925f983340f975a47c636476e0f
HassanBahati <mukisabahati@gmail.com>
2022-02-09 19:21:04 +0300

    add about route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '/src/components/Home.vue'
+import About from '/src/components/About.vue'
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
   },
 ];
 
